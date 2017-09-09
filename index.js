f20bcc0be0323cac29078451bde6a7fe9c78983d
Moises Romero <moises@novacloudware.com>
2017-09-09 13:13:43 -0700

    Small different way to import logger

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Environment = require('./src/Environment');
-const {logger} = require('ezzy-logger');
+const logger = require('ezzy-logger').logger;
 
 const environment = Environment.inst;
 logger.info({title: 'Environment', message: environment});
