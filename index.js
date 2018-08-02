aa412d2f1b23eb1b6d9893d5332c2b6d96862de2
Moises <ezzygemini@gmail.com>
2018-08-02 05:47:16 -0700

    Better logging.

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 const Environment = require('./src/Environment');
-const logger = require('ezzy-logger').logger;
-
-const environment = Environment.inst;
-logger.info({title: 'Environment', message: environment});
-module.exports = environment;
+module.exports = Environment.inst;
